55331f89adfff92957bad9e0d60a370bd8a57733
Dzoel31 <dzulfikriadjmal@gmail.com>
2023-07-18 15:28:01 +0700

    UPDATE: Added calculation of IP scores per semester and IP charts

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os, pandas as pd
-from collections import namedtuple
+from collections import namedtuple, OrderedDict
 from prettytable import PrettyTable
+import matplotlib.pyplot as plt
 
 FILENAME = ".\\data_nilai_semester.csv"
 COLUMNS_NAME = ["Semester", "Mata Kuliah", "Jumlah SKS", "Nilai"]
@@ -16,7 +17,7 @@ ANGKAMUTU = {
         "E" : 0
     }
 
-def checkFile() -> bool:
+def checkFile():
     if os.path.exists(FILENAME):
         return True
     else:
@@ -26,18 +27,20 @@ def checkFile() -> bool:
 def readData():
     readData = pd.read_csv(f"{FILENAME}")
     currentData = readData.values.tolist()
+    semester = readData["Semester"].drop_duplicates().tolist()
     if currentData:
-        semester, mataKuliah, SKS, nilai = map(list, zip(*currentData))
+        semesterDuplicate, mataKuliah, SKS, nilai = map(list, zip(*currentData))
     else:
-        semester, mataKuliah, SKS, nilai = ([] for _ in range(4))
+        semesterDuplicate, mataKuliah, SKS, nilai = ([] for _ in range(4))
     dataNilai = namedtuple("dataNilai", [
         "currentData",
+        "semesterDuplicate",
         "semester",
         "mataKuliah",
         "SKS",
         "nilai"])
     
-    return dataNilai (currentData, semester, mataKuliah, SKS, nilai)
+    return dataNilai (currentData, semesterDuplicate, semester, mataKuliah, SKS, nilai)
 
 def writeData(new_data):
     data = readData()
@@ -47,6 +50,16 @@ def writeData(new_data):
 
     saveData = pd.DataFrame(tempData,columns=COLUMNS_NAME)
     saveData.to_csv(f"{FILENAME}", index=False)
+
+def newData(urutanSemester, indeks):
+    os.system("cls")
+    print(f"Input data mata kuliah ke-{indeks + 1}")
+    mataKuliah = input("Nama mata kuliah : ").title()
+    SKS = int(input("Jumlah SKS : "))
+    nilai = input("Nilai : ").upper()
+    data = [urutanSemester, mataKuliah, SKS, nilai]
+
+    return data
 
 def inputData():
     data = readData()
@@ -58,12 +71,8 @@ def inputData():
         urutanSemester = int(input("Semester : "))
     
     banyakMataKuliah = int(input("Banyak mata kuliah : "))
-    for _ in range(banyakMataKuliah):
-        mataKuliah = input("Nama mata kuliah : ")
-        SKS = int(input("Jumlah SKS : "))
-        nilai = input("Nilai : ")
-        
-        tempData = [urutanSemester, mataKuliah, SKS, nilai]
+    for i in range(banyakMataKuliah):
+        tempData = newData(urutanSemester, i)
         
         listData.append(tempData)
     
@@ -72,32 +81,75 @@ def inputData():
 def hitungIPK():
     data = readData()
     totalSKS = 0
-    totalIndeksPrestasi = 0
+    totalSKSxM = 0
 
     for i in range(len(data.currentData)):
         totalSKS += data.SKS[i]
-        totalIndeksPrestasi += ANGKAMUTU[data.nilai[i]] * data.SKS[i]
+        totalSKSxM += ANGKAMUTU[data.nilai[i]] * data.SKS[i]
     
-    IPK = totalIndeksPrestasi / totalSKS
+    IPK = totalSKSxM / totalSKS
     
     return IPK
 
+def hitungIP(semester):
+    data = readData()
+
+    jumlah = data.semesterDuplicate.count(semester)
+    indeks = data.semesterDuplicate.index(semester)
+    totalSKS = 0
+    totalSKSxM = 0
+
+    for i in range(indeks, indeks + jumlah):
+        totalSKS += data.SKS[i]
+        totalSKSxM += ANGKAMUTU[data.nilai[i]] * data.SKS[i]
+    
+    IP = totalSKSxM / totalSKS
+    return IP
 
 def showData():
+    os.system("cls")
     data = readData()
     IPK = hitungIPK()
-    print("="*20)
-    print("|{:<15}{}|".format("IPK Anda : ", IPK))
-    print("="*20)
     
+    tableIPK = PrettyTable()
+    tableIPK.field_names = ["IPK Anda"]
+    tableIPK.add_row(["{:.2f}".format(IPK)])
+    
+    print(tableIPK)
+
     table = PrettyTable()
     table.field_names = ["Semester", "Mata Kuliah", "SKS", "Nilai", "Mutu", "SKS x Nilai"]
+    table.align["Mata Kuliah"] = "l"
+    for semester in data.semester:
+        jumlah = data.semesterDuplicate.count(semester)
+        indeks = data.semesterDuplicate.index(semester)
+        IP = hitungIP(semester)
 
-    for i in range(len(data.currentData)):
-        SKSxNilai = data.SKS[i] * ANGKAMUTU[data.nilai[i]]
-        table.add_row([data.semester[i],data.mataKuliah[i], data.SKS[i], data.nilai[i], ANGKAMUTU[data.nilai[i]], SKSxNilai])
+        for i in range(indeks, indeks + jumlah):
+            SKSxNilai = data.SKS[i] * ANGKAMUTU[data.nilai[i]]
+            table.add_row([semester ,data.mataKuliah[i], data.SKS[i], data.nilai[i], ANGKAMUTU[data.nilai[i]], SKSxNilai])
+        print(table)
+        table.clear_rows()
+
+        tableIP = PrettyTable()
+        tableIP.field_names = [f"IP Semester {semester}"]
+        tableIP.add_row(["{:.2f}".format(IP)])
+        print(tableIP)
+
+def showChart():
+    data = readData()
+    listIP = []
+
+    for semester in data.semester:
+        IP = hitungIP(semester)
+        listIP.append(round(IP, 2))
     
-    print(table)
+    print("Menampilkan grafik...")
+    plt.plot(data.semester, listIP, marker = "o")
+    plt.title("Grafik IPK")
+    plt.xlabel("Semester")
+    plt.ylabel("IP")
+    plt.show()
 
 def main():
     while True:
@@ -108,7 +160,8 @@ def main():
         print("-"*30)
         print("[1] Input Nilai")
         print("[2] Lihat IP & IPK")
-        print("[3] Exit")
+        print("[3] Lihat Grafik")
+        print("[0] Exit")
         choose = int(input("Pilih menu: "))
 
         if choose == 1:
@@ -116,6 +169,8 @@ def main():
         elif choose == 2:
             showData()
         elif choose == 3:
+            showChart()
+        elif choose == 0:
             print("Exit program...")
             exit()
         else:
